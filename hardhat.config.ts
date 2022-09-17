192393f5d2b11e770f59d0cab0566ca0b2edea5b
崔棉大师 <1046166@qq.com>
2022-09-17 08:08:20 +0800

    update upgrade token script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -770,6 +770,31 @@ task("deploy-proxy-token", "deploy contract")
     }
   );
 
+task("update-proxy-token", "deploy contract")
+  .addParam("token", "token proxy address", "")
+  .addParam("rpc", "rpc connect")
+  .addParam("proxyadmin", "proxy admin private key")
+  .setAction(
+    async ({ token, rpc, proxyadmin }, { ethers, run, network }) => {
+      await run("compile");
+
+      let provider = new ethers.providers.JsonRpcProvider(rpc);
+      const proxyWallet = new ethers.Wallet(proxyadmin, provider);
+
+      const proxy = await ethers.getContractAt("TransparentUpgradeableProxy", token, proxyWallet) as TransparentUpgradeableProxy;
+      // impl
+      const impl = await (
+        await (
+          await ethers.getContractFactory("ERC20MintablePauseableUpgradeable", proxyWallet)
+        ).deploy()
+      ).deployed() as ERC20MintablePauseableUpgradeable;
+      console.log("impl:", impl.address);
+
+      let receipt = await proxy.upgradeTo(impl.address);
+      console.log("upgradeTo Tx:", receipt.hash);
+
+    }
+  );
 task("regresid", "get resource ID")
   .setAction(
     async ({ }, { ethers, run, network }) => {

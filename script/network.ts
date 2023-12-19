943578fe5f0a5c706fdfd8993adb073d606a2084
Xiaohan Zhu <zhuxh@hotmail.com>
2023-12-18 16:52:19 -0800

    fix network settings for consistency

--- script/network.ts
+++ script/network.ts
@@ -10,7 +10,9 @@ export const RPCS = {
   ganache: {
     url: `http://127.0.0.1:7545`,
     chainId: 1337,
-    accounts: [process.env.PRIVATE_KEY_4]
+    accounts: {
+      mnemonic: process.env.MNEMONIC,
+    },
   },
   mainnet: {
     url: `https://ethereum.publicnode.com`,
